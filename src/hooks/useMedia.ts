4f5dc469e0f7698e10bb0ec027ecbf2d54aad629
Luiz Henrique <luizhjramos@outlook.com>
2022-02-01 23:13:17 -0300

    feat: getValue function

--- src/hooks/useMedia.ts
+++ src/hooks/useMedia.ts
@@ -1,4 +1,12 @@
 const useMediaQuery = <T>(queries: string[], values: T[], defaultValue: T) => {
-    // Array contendo a lista de mediaqueries
-    const mediaQuerieList = queries.map(query => window.matchMedia(query))
-}
+  // Array contendo a lista de mediaqueries
+  const mediaQuerieList = queries.map((query) => window.matchMedia(query));
+
+  // função que vai retornar o valor baseado na media query
+  const getValue = () => {
+    const index = mediaQuerieList.findIndex((mql) => mql.matches);
+    return values?.[index] || defaultValue;
+  };
+};
+
+export default useMediaQuery;
