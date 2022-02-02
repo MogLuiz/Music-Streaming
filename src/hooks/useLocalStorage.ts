e48c40e0fd43d6453dc1df39a429b871c4bfc29b
Luiz Henrique <luizhjramos@outlook.com>
2022-02-01 21:06:46 -0300

    feat: setStoragedValue function to setting my localStorage

--- src/hooks/useLocalStorage.ts
+++ src/hooks/useLocalStorage.ts
@@ -13,4 +13,16 @@ export const useLocalStorage = <T>(key: string, initialValue: T) => {
       return initialValue;
     }
   });
+
+  const setValue = (value: T | ((value: T) => T)) => {
+    try {
+      const valueToStore =
+        value instanceof Function ? value(storagedValue) : value;
+      setStoragedValue(valueToStore);
+
+      window.localStorage.setItem(key, JSON.stringify(valueToStore));
+    } catch (error) {
+      console.log(error);
+    }
+  };
 };
