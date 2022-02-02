bbc5622826af11daa432563dcac4cbd1a088ddcb
Luiz Henrique <luizhjramos@outlook.com>
2022-02-01 23:23:45 -0300

    feat: adding listeners

--- src/hooks/useMedia.ts
+++ src/hooks/useMedia.ts
@@ -1,3 +1,6 @@
+// Hooks
+import { useEffect, useState } from "react";
+
 const useMediaQuery = <T>(queries: string[], values: T[], defaultValue: T) => {
   // Array contendo a lista de mediaqueries
   const mediaQuerieList = queries.map((query) => window.matchMedia(query));
@@ -7,6 +10,18 @@ const useMediaQuery = <T>(queries: string[], values: T[], defaultValue: T) => {
     const index = mediaQuerieList.findIndex((mql) => mql.matches);
     return values?.[index] || defaultValue;
   };
+
+  const [value, setValue] = useState<T>(getValue);
+
+  useEffect(() => {
+    const handler = () => setValue(getValue);
+
+    mediaQuerieList.forEach((mql) => mql.addListener(handler));
+    return () =>
+      mediaQuerieList.forEach((mql) => mql.addListener(handler));
+  });
+
+  return value
 };
 
 export default useMediaQuery;
